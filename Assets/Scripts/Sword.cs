34f5665220ed10bcf0e3d7b072a6a488db5664ed
CBEvans <Silversonic21beast@gmail.com>
2026-09-02 09:48:57 -0500

    WIP

---
+++ Assets/Scripts/Sword.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class Sword : MonoBehaviour
+{
+
+    public Sword sword;
+    public float damage = 6f;
+    public float attackSpeed = 2f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
