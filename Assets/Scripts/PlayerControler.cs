2c09533c69a037758eed7028d98d195405270395
CBEvans <Silversonic21beast@gmail.com>
2026-09-02 17:44:44 -0500

    Invert Controls

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -2,12 +2,10 @@ using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
 
-
 [RequireComponent(typeof(CharacterController))]
-
 public class PlayerControler : MonoBehaviour
 {
-   public Camera playerCamera;
+    public Camera playerCamera;
     public float walkSpeed = 6f;
     public float runSpeed = 12f;
     public float jumpPower = 7f;
@@ -23,6 +21,8 @@ public class PlayerControler : MonoBehaviour
     private CharacterController characterController;
 
     private bool canMove = true;
+    private bool isRunning = false;
+
 
     void Start()
     {
@@ -36,13 +36,29 @@ public class PlayerControler : MonoBehaviour
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 right = transform.TransformDirection(Vector3.right);
 
-        bool isRunning = Input.GetKey(KeyCode.LeftShift);
-        float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
-        float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
+         if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            isRunning = !isRunning;
+        }
+        
+        float speed = canMove ? (isRunning ? runSpeed : walkSpeed) : 0f;
+
+
+        float curSpeedX = 0f;
+        float curSpeedY = 0f;
+
+        // ⭐ Individual WASD key checks
+        if (Input.GetKey(KeyCode.S)) curSpeedX = speed;
+        if (Input.GetKey(KeyCode.W)) curSpeedX = -speed;
+        if (Input.GetKey(KeyCode.A)) curSpeedY = speed;
+        if (Input.GetKey(KeyCode.D)) curSpeedY = -speed;
+
         float movementDirectionY = moveDirection.y;
+
         moveDirection = (forward * curSpeedX) + (right * curSpeedY);
 
-        if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
+        // Jump
+        if (Input.GetKey(KeyCode.LeftShift) && canMove && characterController.isGrounded)
         {
             moveDirection.y = jumpPower;
         }
@@ -51,17 +67,18 @@ public class PlayerControler : MonoBehaviour
             moveDirection.y = movementDirectionY;
         }
 
+        // Gravity
         if (!characterController.isGrounded)
         {
             moveDirection.y -= gravity * Time.deltaTime;
         }
 
-        if (Input.GetKey(KeyCode.R) && canMove)
+        // Crouch
+        if (Input.GetButton("Jump") && canMove)
         {
             characterController.height = crouchHeight;
             walkSpeed = crouchSpeed;
             runSpeed = crouchSpeed;
-
         }
         else
         {
@@ -70,14 +87,16 @@ public class PlayerControler : MonoBehaviour
             runSpeed = 12f;
         }
 
+        // Apply movement
         characterController.Move(moveDirection * Time.deltaTime);
 
+        // Mouse look
         if (canMove)
         {
-            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+            rotationX -= -Input.GetAxis("Mouse Y") * lookSpeed;
             rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
             playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
-            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+            transform.rotation *= Quaternion.Euler(0, -Input.GetAxis("Mouse X") * lookSpeed, 0);
         }
     }
 }
