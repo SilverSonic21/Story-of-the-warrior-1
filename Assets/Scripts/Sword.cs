67602092c14103a0e1ae145d856999edadbafc1d
CBEvans <Silversonic21beast@gmail.com>
2026-09-03 19:39:23 -0500

    Sword Works

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -5,6 +5,7 @@ using System.Collections;
 public class Sword : MonoBehaviour
 {
 
+    public Animator swordSwing;
     public Sword sword;
     public float damage = 6f;
     public float attackSpeed = 2f;
@@ -13,10 +14,19 @@ public class Sword : MonoBehaviour
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            StartCoroutine(SwingSword());
+        }
     }
+
+    IEnumerator SwingSword()
+    {
+        swordSwing.SetBool("Sword", true);
+        yield return new WaitForSeconds(1f / attackSpeed);
+        swordSwing.SetBool("Sword", false);
+    }
+  
 }
